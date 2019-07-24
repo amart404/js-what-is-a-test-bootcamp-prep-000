d9de80a3d9108abc8b1c3f804a98dcfb1e2b657e
Aaron Martinez <aaronmarti17@gmail.com>
2019-07-24 18:57:12 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,17 +1,17 @@
 var index = require("../index.js")
 
-
+/*
 describe('what-is-a-test', () => {
   describe('Name', () => {
-    it('returns "Joe"', () => {
-      expect(index.name).toEqual('Joe')
+    it('returns "Susan"', () => {
+      expect(index.name).toEqual('Susan')
     })
   })
 
 
   describe('Height', () => {
-    it('is less than 80', () => {
-      expect(index.height).toBeLessThan(80)
+    it('is less than 40', () => {
+      expect(index.height).toBeLessThan(40)
     })
   })
 
